9dfed4668db47a5bd0873df040d41bac9688d9ee
NascimentoTales <nascimentosantostales@gmail.com>
2022-08-29 19:02:43 -0300

    js finished

--- src/scripts/index/index.js
+++ src/scripts/index/index.js
@@ -1,31 +1,52 @@
 const navbar = document.querySelector(".navbar")
 const navbarOfsetTop = navbar.offsetTop
 
+const sections = document.querySelectorAll('section')
+const navbarLinks = document.querySelectorAll('.navbar-link')
 
-window.addEventListener('scroll', (e) => {
+const progress = document.querySelector('.progress-bars-wrapper')
+// const progressBarSpans = document.querySelectorAll('.progress-bar span')
+const progressBarPercents = [98, 90, 83, 93, 80, 85]
 
-    e.preventDefault()
+
+window.addEventListener('scroll', () => {
+    mainFn();
+})
+
+const mainFn = () => {
 
     if(window.scrollY >= navbarOfsetTop ){
         navbar.classList.add('sticky')
     }else{
         navbar.classList.remove('sticky')
     }
-})
 
-// 
-
-const sections = document.querySelectorAll('section')
-const navbarLinks = document.querySelectorAll('.navbar-link')
-
-sections.forEach( (section, i) => {
+    sections.forEach( (section, i) => {
     
-    if (window.pageYOffset >= section.offsetTop - 10) {
+        if (window.scrollY >= section.offsetTop - 10) {
+    
+            navbarLinks.forEach(navbarlink => {
+                navbarlink.classList.remove('change')
+            });
+    
+            navbarLinks[i].classList.add('change');
+        }
+    })
 
-        navbarLinks.forEach(navbarlink => {
-            navbarlink.classList.remove('change')
-        });
-
-        navbarLinks[i].classList.add('change');
+    if (window.scrollY + window.innerHeight >= progress.offsetTop) {
+        document.querySelectorAll('.progress-porcent').forEach((el, i) => {
+            el.style.width = `${progressBarPercents[i]}%`
+            el.previousElementSibling.firstElementChild.textContent = progressBarPercents[i]
+        })
+        // progressBarSpans.forEach((sp, i) => {
+        //     sp.innerHTML = `${progressBarPercents[i]}`
+        // })
     }
+}
+
+mainFn()
+
+window.addEventListener('resize', () => {
+    window.location.reload();
 })
+
